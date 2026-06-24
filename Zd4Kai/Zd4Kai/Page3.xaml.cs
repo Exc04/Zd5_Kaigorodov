db13a23fa38f84e991ba22ccb387ade287bf0c42
Exc04 <makskaigorodov54@gmail.com>
2026-06-24 11:08:19 +0500

    Zd5.1

---
+++ Zd4Kai/Zd4Kai/Page3.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Zd4Kai
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page3 : ContentPage
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        private async void SignIn(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NameEntry.Text) || string.IsNullOrWhiteSpace(PasswordEntry.Text))
+            {
+                await DisplayAlert("Ошибка", "Заполните все поля", "OK"); return;
+            }
+            await Navigation.PushAsync(new MainPage());
+
+            string[] userCredentials = { NameEntry.Text, PasswordEntry.Text };
+
+            MessagingCenter.Send(this, "UserSignedIn", userCredentials);
+
+        }
+    }
+}
